4bd9b61b9e97e5b48cadced35babbc6f428e07a5
humbertoragazzini <beto_vicio@hotmail.com>
2022-03-20 20:00:57 +0000

    starting with python serial code base36

--- serial_generator/views.py
+++ serial_generator/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def serial_generator(request):
-    """ render home page """
-    return render(request, 'serial_generator/serial_generator.html')
+    cache= 0
+    
+    if request.method == 'GET':
+        form = request.GET
+        print(form['product_serial'])
+    
+    context = {
+            "cache": cache,
+        }
+
+    return render(request, 'serial_generator/serial_generator.html', context)
